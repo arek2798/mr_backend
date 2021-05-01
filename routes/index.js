c675fa55f8eec046c970ec8197d1ce8a3fea8895
Arkadiusz Kuptz <arekk2798@outlook.com>
2021-05-01 02:11:32 +0200

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+
+const router = express.Router();
+
+router.get('/test', (req, res) => {
+    res.json({
+        hello: "hi!"
+    });
+})
+
+module.exports = router;
